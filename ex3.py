3a61167d64e460518a3a426400566b5b54fa6a65
Jeehoon Yoo <jihungen@gmail.com>
2017-02-26 21:40:44 +0900

    Initial commit.

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# Just print the sentence, kind of openings.
+print "I will now count my chickens:"
+
+# Show the number of Hens and Roosters using math operands.
+print "Hens", 25.0 + 30.0 / 6.0
+# * and % seem to have equal priority in calculation.
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+# Calculate the % and / first!
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+# Boolean operand seems to have lower priority than + or -
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# Returns the boolean values.
+print "Is is greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
---
+++ ex3extra.py
@@ -0,0 +1,3 @@
+print 1 + 2 + 3 / 2
+print "It is 7 / 4 result: ", 7 / 4
+print "It is 7.0 / 4/0 result: ", 7.0 / 4.0
